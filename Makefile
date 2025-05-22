eb3d816e10cf722ad3ddcceeab4ab0e12f6fc67f
Nebelschwimmer <nebelschwimmer@gmail.com>
2025-05-22 08:43:24 +0300

    fix: sql, makefile

--- Makefile
+++ Makefile
@@ -4,20 +4,8 @@ DB_CONTAINER_NAME=db
 MIGRATIONS_PATH=db/create-tables.sql
 UPLOADS_PATH=/public/uploads
 
-.PHONY: prepare run run_air init_db init_prod init_dev prod dev
+.PHONY: init
 
-init_prod: prepare init_db prod
-init_dev: prepare init_db dev
-
-# prepare:
-# 	# mkdir -p $(UPLOADS_PATH)
-# 	chmod -R 755 $(UPLOADS_PATH)
-
-init_db:
+init:
 	docker exec -i $(DB_CONTAINER_NAME) psql -U $(DB_USER) -d $(DB_NAME) < $(MIGRATIONS_PATH)
 
-prod:
-	go run .
-
-dev:
-	air
--- db/create-tables.sql
+++ db/create-tables.sql
@@ -1,15 +1,5 @@
-CREATE TABLE book (
-    id SERIAL NOT NULL,
-    title VARCHAR(128) NOT NULL,
-    release_date INT NOT NULL,
-    summary TEXT DEFAULT NULL,
-    price DECIMAL(5,2) NOT NULL,
-    cover VARCHAR(128) NOT NULL,
-    author_id INT DEFAULT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (author_id) REFERENCES author(id)
-    CREATE INDEX book_author_id_idx ON book (author_id)
-);
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS author;
 
 CREATE TABLE author (
     id SERIAL NOT NULL,
@@ -18,3 +8,17 @@ CREATE TABLE author (
     birthday DATE NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE book (
+    id SERIAL NOT NULL,
+    title VARCHAR(128) NOT NULL,
+    release_year INT NOT NULL,
+    summary TEXT DEFAULT NULL,
+    price DECIMAL(5,2) NOT NULL,
+    cover VARCHAR(128) NOT NULL,
+    author_id INT DEFAULT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (author_id) REFERENCES author(id)
+);
+
+CREATE INDEX book_author_id_idx ON book (author_id);
